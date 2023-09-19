2240e6c8dbc62dd464a991fa6089efd9422c0d30
dhuiPark <hasong925@gmail.com>
2023-09-19 19:37:31 +0900

    Initial Commit with Firebase configuration

--- Simple-ToDo/ViewController.swift
+++ Simple-ToDo/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    var ref: DatabaseReference?
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ref = Database.database().reference()
     }
 
 
