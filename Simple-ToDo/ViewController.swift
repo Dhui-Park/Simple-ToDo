00b41bd2bf925fea012ebc53969c9d713b610364
dhuiPark <hasong925@gmail.com>
2023-09-21 13:52:56 +0900

    Firebase CRUD

--- Simple-ToDo/ViewController.swift
+++ Simple-ToDo/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseDatabase
 
 struct TodoEntity {
+    var refId: String
     var todo: String
     var isDone: Bool = false
 }
@@ -26,27 +27,147 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        ref = Database.database().reference()
+        ref = Database
+            .database(url: "https://simple-todo-3b7ec-default-rtdb.asia-southeast1.firebasedatabase.app")
+            .reference().child("todos")
         
         self.todoTableView.dataSource = self
         self.todoTableView.delegate = self
+        
+        // 값 전체를 옵저빙(관찰)중.
+//        ref?.observe(.value) { snapshot in
+//            
+//            self.todoList = []
+//            
+//            for child in snapshot.children {
+//                guard let childSnapShot = child as? DataSnapshot else { return }
+//                
+//                let value = childSnapShot.value as? NSDictionary
+//                let todo = value?["todo"] as? String ?? ""
+//                let isDone = value?["isDone"] as? Bool ?? false
+//                
+//                let fetchedTodoEntity = TodoEntity(refId: childSnapShot.key,
+//                                                   todo: todo,
+//                                                   isDone: isDone)
+//                print(#fileID, #function, #line, "- fetchedTodoEntity: \(fetchedTodoEntity)")
+//                self.todoList.append(fetchedTodoEntity)
+//            }
+//            self.todoTableView.reloadData()
+//        }
+        
+        // 데이터 한번만 받기
+        
+//        ref?.getData(completion: { error, snapshot in
+//            if let error = error {
+//                print(#fileID, #function, #line, "- error: \(error)")
+//                return
+//            }
+//            
+//            guard let snapshot = snapshot else { return }
+//            
+//            for child in snapshot.children {
+//                guard let childSnapShot = child as? DataSnapshot else { return }
+//                
+//                let value = childSnapShot.value as? NSDictionary
+//                let todo = value?["todo"] as? String ?? ""
+//                let isDone = value?["isDone"] as? Bool ?? false
+//                
+//                let fetchedTodoEntity = TodoEntity(refId: childSnapShot.key,
+//                                                   todo: todo,
+//                                                   isDone: isDone)
+//                print(#fileID, #function, #line, "- fetchedTodoEntity: \(fetchedTodoEntity)")
+//                self.todoList.append(fetchedTodoEntity)
+//            }
+//            self.todoTableView.reloadData()
+//            
+//        })
+        
+        // Listen for deleted comments in the Firebase database
+        // 삭제가 일어났을 때만 받겠다
+        ref?.observe(.childRemoved, with: { (snapshot) -> Void in
+            
+            guard let index: Int = self.todoList.firstIndex(where: { $0.refId == snapshot.key }) else { return }
+            
+            self.todoList.remove(at: index)
+            
+            let removingIndexPath = IndexPath(row: index, section: 0)
+            
+            self.todoTableView.deleteRows(at: [removingIndexPath], with: .fade)
+            
+            
+        })
+        
+        // Listen for new comments in the Firebase database
+        // 데이터 추가가 일어났을 때만 받겠다
+        ref?.observe(.childAdded, with: { (snapshot) -> Void in
+            
+            let value = snapshot.value as? NSDictionary
+            let todo = value?["todo"] as? String ?? ""
+            let isDone = value?["isDone"] as? Bool ?? false
+            
+            let addedTodoEntity = TodoEntity(refId: snapshot.key,
+                                               todo: todo,
+                                               isDone: isDone)
+            print(#fileID, #function, #line, "- addedTodoEntity: \(addedTodoEntity)")
+            
+            // 1. 데이터 추가
+            self.todoList.append(addedTodoEntity)
+            
+            let appendingIndexPath = IndexPath(row: self.todoList.count - 1, section: 0)
+            
+            self.todoTableView.insertRows(at: [appendingIndexPath], with: .fade)
+        })
+        
+        // 특정 데이터가 변경되었을 때만 받겠다
+        ref?.observe(.childChanged, with: { (snapshot) -> Void in
+            
+            guard let index: Int = self.todoList.firstIndex(where: { $0.refId == snapshot.key }) else { return }
+            
+            let value = snapshot.value as? NSDictionary
+            let todo = value?["todo"] as? String ?? ""
+            let isDone = value?["isDone"] as? Bool ?? false
+            
+            let changedTodoEntity = TodoEntity(refId: snapshot.key,
+                                               todo: todo,
+                                               isDone: isDone)
+            print(#fileID, #function, #line, "- changedTodoEntity: \(changedTodoEntity)")
+            
+            // 1. 데이터 변경
+            self.todoList[index] = changedTodoEntity
+            
+            let changingIndexPath = IndexPath(row: index, section: 0)
+            
+            self.todoTableView.reloadRows(at: [changingIndexPath], with: .fade)
+        })
+        
+        
     }
-
+    
     @IBAction func addTodoBtnClicked(_ sender: UIButton) {
         print(#fileID, #function, #line, "- ")
         guard let userInput: String = todoInputTextField.text,
-        userInput.count > 0 else {
+              userInput.count > 0 else {
             presentAlert()
             return
         }
         
-        let newTodo = TodoEntity(todo: userInput, isDone: false)
+//        let newTodo = TodoEntity(todo: userInput, isDone: false)
+
+        // 데이터 추가되는 부분 -> 그런 다음은? viewDidLoad()에 수신하는 놈을 하나 만들자.
+        self.ref?
+//            .child("3")
+            .childByAutoId()
+            .setValue([
+                "todo": userInput,
+                "isDone": false
+            ] as [String : Any])
         
-        self.todoList.append(newTodo)
         
-        let newIndexPath = IndexPath(row: self.todoList.count - 1, section: 0)
+//        self.todoList.append(newTodo)
         
-        self.todoTableView.insertRows(at: [newIndexPath], with: .fade)
+//        let newIndexPath = IndexPath(row: self.todoList.count - 1, section: 0)
+//
+//        self.todoTableView.insertRows(at: [newIndexPath], with: .fade)
         
         self.todoInputTextField.text = ""
     }
@@ -83,8 +204,13 @@ class ViewController: UIViewController {
             guard let self = self,
                   let userInput = inputTF?.text else { return }
             
-            self.todoList[indexPath.row].todo = userInput
-            self.todoTableView.reloadRows(at: [indexPath], with: .fade)
+            let editingTodo = self.todoList[indexPath.row]
+            
+            self.ref?.child(editingTodo.refId)
+                .updateChildValues(["todo": userInput], withCompletionBlock: {_,_ in })
+            
+//            self.todoList[indexPath.row].todo = userInput
+//            self.todoTableView.reloadRows(at: [indexPath], with: .fade)
         })
         let cancelAction = UIAlertAction(title: NSLocalizedString("닫기", comment: "Default action"),
                                          style: .cancel,
@@ -115,9 +241,16 @@ extension ViewController: UITableViewDataSource {
         cell.indexPath = indexPath
         
         cell.isDoneAction = { [weak self] indexPath, isDone in
+            
+            guard let self = self else { return }
             print(#fileID, #function, #line, "- indexPath: \(indexPath), isDone: \(isDone)")
+            
+            let isDoneCheckingTodo = self.todoList[indexPath.row]
+            
+            self.ref?.child(isDoneCheckingTodo.refId)
+                .updateChildValues(["isDone": isDone], withCompletionBlock: {_,_ in })
             // 1. 데이터 바꾸기
-            self?.todoList[indexPath.row].isDone = isDone
+//            self?.todoList[indexPath.row].isDone = isDone
             // 2. UI 업데이트
 //            tableView.reloadRows(at: [indexPath], with: .fade)
         }
@@ -133,10 +266,18 @@ extension ViewController: UITableViewDelegate {
         
         let deleteAction = UIContextualAction(style: .destructive, title: "삭제", handler: { _,_,_  in
             print(#fileID, #function, #line, "- 삭제: \(indexPath)")
-            // 1. 데이터 지우기
-            self.todoList.remove(at: indexPath.row)
-            // 2. 셀 리로드 혹은 셀 지우기
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            let todoToBeDeleted = self.todoList[indexPath.row]
+
+            self.ref?
+                .child(todoToBeDeleted.refId)
+                .removeValue()
+            
+            
+//            // 1. 데이터 지우기
+//            self.todoList.remove(at: indexPath.row)
+//            // 2. 셀 리로드 혹은 셀 지우기
+//            tableView.deleteRows(at: [indexPath], with: .fade)
         })
         
         let cellConfig = UISwipeActionsConfiguration(actions: [deleteAction])
